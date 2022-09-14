c704019d5d6dcfacd9588e45a75f092bdf81fee2
ogbuagujoseph <ogbuagujoseph@gmail.com>
2022-09-14 16:46:08 +0100

    update file

--- api/settings.py
+++ api/settings.py
@@ -128,8 +128,8 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]  # new
-STATIC_ROOT = BASE_DIR / "staticfiles"  # new
+# STATICFILES_DIRS = [BASE_DIR / "static"]  # new
+# STATIC_ROOT = BASE_DIR / "staticfiles"  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
